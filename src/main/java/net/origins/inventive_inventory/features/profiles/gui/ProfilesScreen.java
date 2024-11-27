c77debe9305e5aa63f387e8a2ca4b6bc4ba23bc6
LU-XD <114474715+LU-XD@users.noreply.github.com>
2024-11-27 14:52:08 +0100

    1.1.4

--- src/main/java/net/origins/inventive_inventory/features/profiles/gui/ProfilesScreen.java
+++ src/main/java/net/origins/inventive_inventory/features/profiles/gui/ProfilesScreen.java
@@ -116,7 +116,7 @@ public class ProfilesScreen extends HandledScreen<ScreenHandler> {
             Profile profile = sections.get(section).getProfile();
             if (profile == null) {
                 if (DELETE_KEY_PRESSED) {
-                    InventiveInventory.getClient().setScreen(new ProfilesNamingScreen());
+                    client.send(() -> client.setScreen(new ProfilesNamingScreen()));
                 } else {
                     ProfileHandler.create("", ProfileHandler.getAvailableProfileKey());
                 }
