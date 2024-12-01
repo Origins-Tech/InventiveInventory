589a7820c07b789295ca187816ed1edc07a6138e
Strobel273 <moritz.strobel@outlook.de>
2024-12-01 23:11:07 +0100

    remove unnecessary drawer function

--- src/main/java/net/origins/inventive_inventory/config/screens/widgets/DrawerFunction.java
+++
@@ -1,8 +0,0 @@
-package net.origins.inventive_inventory.config.screens.widgets;
-
-import net.minecraft.client.gui.DrawContext;
-
-@FunctionalInterface
-public interface DrawerFunction {
-    void draw(DrawContext context, int x, int y);
-}
