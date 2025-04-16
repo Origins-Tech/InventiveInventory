d88fafe21c591fbf7c4bae8e786d45973b3d9946
Disturbtion273 <moritz.strobel@outlook.de>
2025-04-16 21:07:12 +0200

    feat: travelers backpack compatibility

--- src/main/java/net/inventive_mods/inventive_inventory/util/slots/ContainerSlots.java
+++ src/main/java/net/inventive_mods/inventive_inventory/util/slots/ContainerSlots.java
@@ -11,6 +11,9 @@ public class ContainerSlots {
     public static SlotRange get() {
         ScreenHandler screenHandler = InventiveInventory.getScreenHandler();
         List<Slot> containerSlots = screenHandler.slots.stream().filter(slot -> !(slot.inventory instanceof PlayerInventory)).toList();
+        if (screenHandler.getClass().getSimpleName().equals("BackpackBlockEntityMenu")) {
+            containerSlots = containerSlots.stream().filter(slot -> slot.getClass().getSimpleName().equals("BackpackSlotItemHandler")).toList();
+        }
         if (containerSlots.isEmpty()) return new SlotRange(0, 0);
         return new SlotRange(containerSlots.getFirst().id, containerSlots.getLast().id);
     }
